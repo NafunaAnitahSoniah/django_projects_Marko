5112d6e614a7de8b8ca843bd72578deaf46e56a2
NafunaAnitahSoniah <nafunasoniah@gmail.com>
2025-07-08 16:08:04 +0300

    still on the form

--- Demo3/home/views.py
+++ Demo3/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 #box or list of the students
 students = []
@@ -6,19 +6,27 @@ students = []
 # Create your views here.
 def homePage(request):
     if request.method == 'POST':
-        form_data = request.POST
+        #we are creating a variable in which we store the contents that we received from the POST method in the form
+        form_data = request.POST 
+        #We create a variable name that will capture the first field {student-name} from the form
         name = form_data['student_name']
+        #We create a variable lucky_number that will capture the second field {lucky_number} from the form
         lucky_number = form_data['lucky_number']
+
+        #below indicates what we want to do with the data received from the form
         new_student = {
             'name': name,
             'lucky_number': lucky_number
         }
         students.append(new_student)
     
-    total_students = len(students)
-    print("Total Students: " +str(total_students))
+        total_students = len(students)
+        print("Total Students: " +str(total_students))
+    
+        return redirect('/')
+    else:
+        return render(request, "index.html", {"students":students})
 
-    return render(request, "index.html")
 
 """
 def addStudent(request):
